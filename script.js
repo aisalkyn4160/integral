bc38f2172a9b2ff0d19bd191e1a2c9529d85b896
aisalkyn4160 <aisalkyn.4160@gmail.com>
2025-10-04 02:16:16 +0700

    creating main page

--- script.js
+++ script.js
@@ -32,3 +32,33 @@ const categoriesSwiper = new Swiper('.categories-swiper', {
         prevEl: '.categories .swiper-button-prev',
     },
 })
+
+const productCardSwiper = new Swiper('.product-card-swiper', {
+    loop: true,
+    pagination: {
+        el: '.product-card-pagination',
+        clickable: true,
+    },
+})
+
+const newSwiper = new Swiper('.new-products-swiper', {
+    slidesPerView: 'auto',
+    loop: true,
+    spaceBetween: 24,
+})
+const hitSwiper = new Swiper('.hit-products-swiper', {
+    slidesPerView: 'auto',
+    loop: true,
+    spaceBetween: 24,
+})
+const solutionsSwiper = new Swiper('.solutions-swiper', {
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 24,
+})
+
+const newsSwiper = new Swiper('.news-swiper', {
+    slidesPerView: 'auto',
+    loop: true,
+    spaceBetween: 16,
+})
